dd5d9e20424319beb79481cff7e6a036661a34eb
Matthew Jones <mcolinj@mac.com>
2018-01-20 14:15:06 -0500

    Add joystick template code for reference and trimmed down controller.

---
+++ tools/motor-control.py
@@ -0,0 +1,97 @@
+import pygame
+
+# Define some colors
+BLACK    = (   0,   0,   0)
+WHITE    = ( 255, 255, 255)
+LEFT_STICK = 1
+RIGHT_STICK = 3
+
+# This is a simple class that will help us print to the screen
+# It has nothing to do with the joysticks, just outputting the
+# information.
+class TextPrint:
+    def __init__(self):
+        self.reset()
+        self.font = pygame.font.Font(None, 20)
+
+    def print(self, screen, textString):
+        textBitmap = self.font.render(textString, True, BLACK)
+        screen.blit(textBitmap, [self.x, self.y])
+        self.y += self.line_height
+        
+    def reset(self):
+        self.x = 10
+        self.y = 10
+        self.line_height = 15
+        
+    def indent(self):
+        self.x += 10
+        
+    def unindent(self):
+        self.x -= 10
+    
+
+pygame.init()
+ 
+# Set the width and height of the screen [width,height]
+size = [200, 200]
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption("Robot Sim")
+
+#Loop until the user clicks the close button.
+done = False
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+# Initialize the joysticks
+pygame.joystick.init()
+    
+# Get ready to print
+textPrint = TextPrint()
+
+# -------- Main Program Loop -----------
+while done==False:
+    # EVENT PROCESSING STEP
+    for event in pygame.event.get(): # User did something
+        if event.type == pygame.QUIT: # If user clicked close
+            done=True # Flag that we are done so we exit this loop
+        
+        # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
+        if event.type == pygame.JOYBUTTONDOWN:
+            print("Joystick button pressed.")
+        if event.type == pygame.JOYBUTTONUP:
+            print("Joystick button released.")
+            
+ 
+    # DRAWING STEP
+    # First, clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+    screen.fill(WHITE)
+    textPrint.reset()
+
+    joystick = pygame.joystick.Joystick(0)
+    joystick.init()
+    
+    # Get the name from the OS for the controller/joystick
+    name = joystick.get_name()
+    textPrint.print(screen, "Joystick: {}".format(name) )
+        
+    right_motor = -joystick.get_axis(RIGHT_STICK)
+    left_motor = -joystick.get_axis(LEFT_STICK)
+        
+    textPrint.print(screen, "Left {:>6.3f} : Right {:>6.3f}".format(left_motor, right_motor) )
+            
+    # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
+    
+    # Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+
+    # Limit to 20 frames per second
+    clock.tick(20)
+    
+# Close the window and quit.
+# If you forget this line, the program will 'hang'
+# on exit if running from IDLE.
+pygame.quit ()
