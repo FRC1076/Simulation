5319611b772604e214b9242ef37da1b2965889c2
Eli <ethds24@gmail.com>
2018-02-14 17:46:02 -0500

    Added second camera

--- UnitySim/Assets/Scripts/buttoncontroller.cs
+++ UnitySim/Assets/Scripts/buttoncontroller.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class buttoncontroller : MonoBehaviour {
 
+
+	bool topCameraEnabled = true;
+	public GameObject topCamera;
+	public GameObject robotCamera;
 	// Use this for initialization
 	void Start () {
-		
+		topCamera.SetActive(true);
+		robotCamera.SetActive(false);
 	}
 	
 	// Update is called once per frame
@@ -15,5 +20,18 @@ public class buttoncontroller : MonoBehaviour {
 	}
 	public void exitGame(){
 		Application.Quit();
+	}
+	public void changeCamera(){
+		if(topCameraEnabled == true){
+			topCamera.SetActive(false);
+			robotCamera.SetActive(true);
+			//Debug.Log("very strange");
+			topCameraEnabled = false;
+
+		}else{
+			topCamera.SetActive(true);
+			robotCamera.SetActive(false);
+			topCameraEnabled = true;
+		}
 	}
 }
